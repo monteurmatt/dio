203bd42944556e37f71c94d47569aafc2ed80406
monteurmatt <gmonteiro.matheus@gmail.com>
2025-06-26 13:01:24 -0300

    Desafio completo

--- java/desafio-simulando-conta-bancaria-terminal/ContaBanco/src/ContaTerminal.java
+++ java/desafio-simulando-conta-bancaria-terminal/ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = sc.nextLine();
+        System.out.print("Digite o número da Conta: ");
+        int numero = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Digite o seu nome: ");
+        String nome = sc.nextLine();
+        System.out.print("Digite o saldo: ");
+        double saldo = sc.nextDouble();
+
+        System.out.println(  String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", nome, agencia, numero, saldo));
+    
+
+        sc.close();
     }
 }
+
